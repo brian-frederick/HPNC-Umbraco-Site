a5825afabb9a3d36a7b8d59668a6842efe0bb84a
Brian Frederick <briandfrederick@gmail.com>
2017-04-15 17:38:08 -0500

    added header and footer and site layout controller

---
+++ HPNC Website/Controllers/SiteLayoutController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+
+namespace HPNC_Website.Controllers
+{
+    public class SiteLayoutController : SurfaceController
+    {
+        private const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/SiteLayout/";
+        public ActionResult RenderHeader()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_header.cshtml");
+        }
+
+        public ActionResult RenderFooter()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_footer.cshtml");
+        }
+    }
+}
