becb292724dc9bba799e57bfd59f5fbeddcede40
Brian Frederick <briandfrederick@gmail.com>
2017-05-20 14:53:43 -0500

    added helper class for adjusting urls of umbraco images, rearranged folds, removed install folder, and published

---
+++ HPNC Website/Models/customURLHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+
+namespace HPNC_Website.Models
+{
+    public class customURLHelper
+    {
+        
+        public static string UmbracoFile(string partialURL)
+        {
+            var BaseURL = ConfigurationManager.AppSettings["BaseURL"];
+
+            var imageIndex = partialURL.IndexOf("/media");
+            var clippedPartialURL = partialURL.Substring(imageIndex);
+
+            var completeURL = BaseURL + clippedPartialURL;
+            return completeURL;
+        }
+    }
+}
